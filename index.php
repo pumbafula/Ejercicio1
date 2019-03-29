e2a085e60d05be1c4d01a95624c9018eb97d33e3
cetecom <cetecom@LCV10418>
2019-03-29 15:51:33 -0300

    Primer avance

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>NoHack</title>
+        <link href="css/estiloprincipal.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div id="contenido">
+            <div id="titulodelsitio"></div>
+            <div id="titulodelapagina"></div>
+            
+        </div>
+
+        <?php
+        ?>
+    </body>
+</html>
